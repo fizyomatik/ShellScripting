cb93a2d41721022bca8ddcbbc697fda5b3f30bd7
mehmet <baykara.e41@gmail.com>
2022-02-02 22:55:14 +0100

    Shell basics

---
+++ Shell1.sh
@@ -0,0 +1,13 @@
+#! /usr/bin/bash
+
+
+echo Whast up?
+
+NAME="Emrullah"
+
+echo "My name is $NAME"
+
+echo "My name is ${NAME}"
+
+read -p "Enter your:   " NAME
+echo "Hello $NAME, nice to meet you"
